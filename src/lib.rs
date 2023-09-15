33f216ddc5f247e63a126a9b20b7641610870978
Abiria <abiria.dev@gmail.com>
2023-09-15 16:24:57 +0900

    feat: try to implement `FromStr`

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: usize, right: usize) -> usize {
-	left + right
+use std::str::FromStr;
+
+use shlex::Shlex;
+
+struct Httpie {
+	url: String,
+	options: Options,
+	headers: Headers,
+	body: Body,
 }
 
-#[cfg(test)]
-mod tests {
-	use super::*;
+struct Options {}
+struct Headers {}
+struct Body;
 
-	#[test]
-	fn it_works() {
-		let result = add(2, 2);
-		assert_eq!(result, 4);
+impl FromStr for Httpie {
+	type Err = ();
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		Shlex::new(s).for_each(|c| println!("{c}"));
+		todo!()
 	}
+}
+
+#[test]
+fn test() {
+	assert!(Httpie::from_str(
+		r#"http --chunked --multipart PUT pie.dev/put hello=world foo@files/data.xml"#,
+	)
+	.is_ok());
 }
